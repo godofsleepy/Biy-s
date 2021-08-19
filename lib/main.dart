cbd6dcdfeffff6c99c5ca968069518994fc90cec
Rifat Khadafy <45101689+godofsleepy@users.noreply.github.com>
2021-08-19 09:20:21 +0700

    complete splash

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
         textTheme: GoogleFonts.montserratTextTheme(
           Theme.of(context).textTheme,
         ),
--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -10,11 +10,15 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController imageAnimationController;
+  late Animation<double> imageAnimation;
+
   @override
   void initState() {
     super.initState();
-
+    setUpAnimation();
     Timer(const Duration(seconds: 2), () {});
   }
 
@@ -23,14 +27,38 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: ResColor.green,
       body: Center(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(100),
-          child: Image.asset(
-            "assets/splash.gif",
-            width: 100,
+        child: ScaleTransition(
+          scale: imageAnimation,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset(
+              "assets/splash.gif",
+              width: 200,
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void setUpAnimation() {
+    imageAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+      value: 0.1,
+    );
+
+    imageAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      parent: imageAnimationController,
+    );
+
+    imageAnimationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    imageAnimationController.dispose();
   }
 }
