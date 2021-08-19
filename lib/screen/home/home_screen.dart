d61965526cd3bc21758e6c40c97159e07e5639b7
Rifat Khadafy <45101689+godofsleepy@users.noreply.github.com>
2021-08-19 13:35:24 +0700

    complete home

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:biys/data/data.dart';
+import 'package:biys/model/base.dart';
+import 'package:biys/utils/resource/rescolor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +14,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Base base;
+
+  @override
+  void initState() {
+    super.initState();
+    base = Base.fromJson(Data.data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,38 +29,111 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Flexible(
-              flex: 3,
-              fit: FlexFit.tight,
-              child: Container(
-                color: Colors.red,
-                padding: EdgeInsets.all(14),
-                child: Text(
-                  "Find\nyour Best\nRestaurants!",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w600,
-                    // color: ResColor.green,
-                  ),
+            Container(
+              padding: EdgeInsets.all(14),
+              child: Text(
+                "Find\nYour Best\nRestaurants!",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                  color: ResColor.yellow,
+                  // color: ResColor.green,
                 ),
               ),
             ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                color: Colors.blue,
-                padding: EdgeInsets.symmetric(horizontal: 14),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Search",
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(14)),
-                    prefixIcon: Icon(Icons.search),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(14),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
                   ),
+                  prefixIcon: Icon(Icons.search, color: Colors.grey[500]),
                 ),
               ),
-            )
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: base.restaurants?.length ?? 0,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 250,
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? 14 : 0,
+                      right:
+                          index == (base.restaurants?.length ?? 1 - 1) ? 14 : 0,
+                    ),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      elevation: 4,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(25.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(300),
+                                ),
+                                child: Image.network(
+                                  base.restaurants?[index].pictureId ?? "",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    base.restaurants?[index].name ?? "",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  RatingBar.builder(
+                                    glowRadius: 0,
+                                    unratedColor: Colors.transparent,
+                                    initialRating:
+                                        (base.restaurants?[index].rating ?? 0),
+                                    direction: Axis.horizontal,
+                                    allowHalfRating: true,
+                                    itemSize: 25,
+                                    itemBuilder: (context, _) => Text(
+                                      "⭐",
+                                      style: TextStyle(
+                                        fontSize: 5,
+                                      ),
+                                    ),
+                                    onRatingUpdate: (rating) => {},
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(width: 8),
+              ),
+            ),
           ],
         ),
       ),
--- lib/utils/resource/rescolor.dart
+++ lib/utils/resource/rescolor.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class ResColor {
   static const Color green = Color(0xffe3fee2);
+  static const Color yellow = Color(0xffefc251);
 }
